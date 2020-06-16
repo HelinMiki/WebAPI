f4e5359b82b497efcef4ca63860f800710caf50e
Miki <mikihelin@gmail.com>
2020-06-16 10:50:38 +0300

    Swashbuckle/Swagger lisätty rajapintaan testausta varten.

---
+++ WebAPI/App_Start/AuthTokenOperation.cs
@@ -0,0 +1,54 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace WebAPI.App_Start
+{
+    //https://stackoverflow.com/questions/51117655/how-to-use-swagger-in-asp-net-webapi-2-0-with-token-based-authentication
+    //IDocumentFilter lisää uuden reitin /token, joka pistetään Auth-kategoriaan. Komento on post.
+    public class AuthTokenOperation : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+        {
+            swaggerDoc.paths.Add("/token", new PathItem 
+            { 
+                post = new Operation
+                {
+                    tags = new List<string> { "Auth" },
+                    consumes = new List<string>
+                    {
+                        "application/x-www-form-urlencoded"
+                    },
+                    parameters = new List<Parameter>
+                    {
+                       new Parameter
+                       {
+                           type = "string",
+                           name = "grant_type",
+                           required = true,
+                           @in = "formData",
+                           @default = "password"
+                       },
+                       new Parameter
+                       {
+                           type = "string",
+                           name = "username",
+                           required = false,
+                           @in = "formData"
+                       },
+                       new Parameter
+                       {
+                           type = "string",
+                           name = "password",
+                           required = false,
+                           @in = "formData"
+                       }
+                    }
+                }
+            });
+        }
+    }
+}
